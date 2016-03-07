eb13131cc20c2c29c55e254c4821c4d9bf5e1eaf
Sam Roberts <sam.roberts.1983@gmail.com>
2016-03-07 23:40:15 +1000

    comment about future test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,5 @@
 main :: IO ()
+-- FIXME: would be nice to compare messages from my protocol
+--        with messages from another Haskell kafka library
+--        quickcheck style
 main = putStrLn "Test suite not yet implemented"
