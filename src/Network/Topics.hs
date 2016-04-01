3d55ea38341b73efee13a9746fbb15274eeda9d1
Sam Roberts <sam.roberts.1983@gmail.com>
2016-04-01 14:57:36 +1000

    reorder Network.Topics

--- src/Network/Topics.hs
+++ src/Network/Topics.hs
@@ -14,8 +14,35 @@ import           Data.Text (Text)
 import           Network.Kafka.Protocol (Deserializable, Serializable)
 import qualified Network.Kafka.Protocol as KP
 
--- FIXME: change request and partition to allow multiple topic * partition pairs per request / response cycle
 -- FIXME: need some way of representing commonalities between different errors
+
+-- | syntax for interacting with kafka
+data Instruction a where
+  GetTopic :: TopicName -> Instruction Topic
+  GetOffsets :: OffsetsRequest -> Instruction OffsetsResponse
+  Produce :: forall v. Kafkaesque v => ProduceRequest v -> Instruction ProduceResponse
+  Fetch :: Kafkaesque v => FetchRequest -> Instruction (FetchResponse v)
+
+instance Show (Instruction Topic) where
+  show (GetTopic name) = "GetTopic " ++ show name
+
+instance Show (Instruction OffsetsResponse) where
+  show (GetOffsets request) = "GetOffsets " ++ show request
+
+instance Show (Instruction ProduceResponse) where
+  show (Produce request) = "Produce " ++ show request
+
+instance Show (Instruction (FetchResponse v)) where
+  show (Fetch request) = "Fetch " ++ show request
+
+type OffsetsRequest = Request ReplicaId ()
+type OffsetsResponse = Response CommonError (Offset, Offset)
+
+type ProduceRequest v = Request ProduceConfig [v]
+type ProduceResponse = Response (Either CommonError FetchError) Offset
+
+type FetchRequest = Request FetchConfig FetchInfo
+type FetchResponse v = Response (Either CommonError FetchError) (FetchData v)
 
 data Request c a = Request
                  { _reqConf :: c
@@ -30,62 +57,47 @@ data Response e a = Response
                   }
                   deriving (Show, Eq, Functor)
 
-class (Show a, Serializable a, Deserializable a) => Kafkaesque a
+data Topic = Topic
+           { _topicName :: TopicName
+           , _topicMaxPartition :: Partition
+           }
+           deriving (Show, Eq)
 
-instance Kafkaesque Int16
-instance Kafkaesque Int32
-instance Kafkaesque Int64
+data ProduceConfig = ProduceConfig
+                   { _requiredAcks :: RequiredAcks
+                   , _timeout :: Timeout
+                   }
+                   deriving (Show, Eq)
 
-data MyType = MyType Int16 Int32 deriving (Show, Eq)
+data FetchConfig = FetchConfig
+                 { _fetchReplicaId :: ReplicaId
+                 , _fetchMaxWaitTime :: Timeout
+                 , _fetchMinBytes :: Size
+                 }
+                 deriving (Show, Eq)
 
-instance Serializable MyType where
-  serialize (MyType i j) = do
-    KP.serialize i
-    KP.serialize j
+data FetchInfo = FetchInfo
+               { _fetchOffset :: Offset
+               , _fetchMaxBytes :: Size
+               }
+               deriving (Show, Eq)
 
-instance Deserializable MyType where
-  deserialize = do
-    i :: Int16 <- KP.deserialize
-    j :: Int32 <- KP.deserialize
-    return $ MyType i j
+data FetchData v = FetchData
+                 { _fetchHighwaterMark :: Offset
+                 , _messages :: [(Offset, v)]
+                 }
+                 deriving (Show, Eq, Functor)
 
-instance Kafkaesque MyType
+data CommonError = Unknown
+                 | UnknownTopicOrPartition
+                 | NotLeaderForPartition
+                 deriving (Show, Eq)
 
-type OffsetsRequest = Request ReplicaId ()
-type OffsetsResponse = Response OffsetError (Offset, Offset)
+data ProduceError = ProduceErrorTODO deriving (Show, Eq)
 
-type ProduceRequest v = Request ProduceConfig [v]
-type ProduceResponse = Response ProduceError Offset
-
-type FetchRequest = Request FetchConfig FetchInfo
-type FetchResponse v = Response FetchError (FetchData v)
-
--- currently thinking of using the operational monad here
-data Instruction a where
-  GetTopic :: TopicName -> Instruction Topic
-  GetOffsets :: OffsetsRequest -> Instruction OffsetsResponse
-  Produce :: forall v. Kafkaesque v => ProduceRequest v -> Instruction ProduceResponse
-  Fetch :: Kafkaesque v => FetchRequest -> Instruction (FetchResponse v)
-
--- Note on GetOffsets:
---
--- Retrieving both offsets works by setting time to the latest and setting max number of offsets to the
--- maximum possible number.
---
--- Might be nice to be able to just retrieve latest or earliest with a targetted request to just retrieve
--- the one offset. Leaving it out of first cut though as I doubt it really makes much difference.
-
-instance Show (Instruction Topic) where
-  show (GetTopic name) = "GetTopic " ++ show name
-
-instance Show (Instruction OffsetsResponse) where
-  show (GetOffsets request) = "GetOffsets " ++ show request
-
-instance Show (Instruction ProduceResponse) where
-  show (Produce request) = "Produce " ++ show request
-
-instance Show (Instruction (FetchResponse v)) where
-  show (Fetch request) = "Fetch " ++ show request
+data FetchError = OffsetOutOfRange
+                | ReplicaNotAvailable
+                deriving (Show, Eq)
 
 newtype TopicName = TopicName Text deriving (Show, Eq, IsString)
 newtype Partition = Partition Int32 deriving (Show, Eq, Ord, Num, Enum, Ix)
@@ -94,26 +106,9 @@ newtype ReplicaId = ReplicaId Int32 deriving (Show, Eq, Ord, Num, Enum, Ix, Boun
 newtype Timeout = Timeout Int32 deriving (Show, Eq, Ord, Num, Enum, Ix)
 newtype Size = Size Int32 deriving (Show, Eq, Ord, Num, Enum, Ix)
 
-data Topic = Topic
-           { _topicName :: TopicName
-           , _topicMaxPartition :: Partition
-           }
-           deriving (Show, Eq)
-
 instance Bounded Partition where
   minBound = 0
   maxBound = Partition maxBound
-
-data OffsetError = OffsetUnknown
-                 | OffsetUnknownTopicOrPartition
-                 | OffsetNotLeaderForPartition
-                 deriving (Show, Eq)
-
-data ProduceConfig = ProduceConfig
-                   { _requiredAcks :: RequiredAcks
-                   , _timeout :: Timeout
-                   }
-                   deriving (Show, Eq)
 
 data RequiredAcks = WaitForNone
                   | WaitForLeader
@@ -121,30 +116,8 @@ data RequiredAcks = WaitForNone
                   | WaitForAtLeast Int16
                   deriving (Show, Eq)
 
-data ProduceError = ProduceErrorTODO deriving (Show, Eq)
+class (Show a, Serializable a, Deserializable a) => Kafkaesque a
 
-data FetchConfig = FetchConfig
-                 { _replicaId :: ReplicaId
-                 , _maxWaitTime :: Timeout
-                 , _minBytes :: Size
-                 }
-                 deriving (Show, Eq)
-
-data FetchInfo = FetchInfo
-               { _fetchOffset :: Offset
-               , _maxBytes :: Size
-               }
-               deriving (Show, Eq)
-
-data FetchError = FetchUnknown
-                | FetchUnknownTopicOrPartition
-                | FetchNotLeaderForPartition
-                | FetchOffsetOutOfRange
-                | FetchReplicaNotAvailable
-                deriving (Show, Eq)
-
-data FetchData v = FetchData
-                 { _highwaterMark :: Offset
-                 , _messages :: [(Offset, v)]
-                 }
-                 deriving (Show, Eq, Functor)
+instance Kafkaesque Int16
+instance Kafkaesque Int32
+instance Kafkaesque Int64
