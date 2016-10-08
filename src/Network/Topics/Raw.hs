3fe715e312f7ec18d9623b6dcee20e68dea148ce
Sam Roberts <sam.roberts.1983@gmail.com>
2016-10-09 00:12:31 +1000

    add in placeholder Raw partition implementation

--- src/Network/Topics/Raw.hs
+++ src/Network/Topics/Raw.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 -- | A topics program which uses does it's own TCP interactions, not relying on a Kafka library
 module Network.Topics.Raw where
@@ -8,9 +11,11 @@ module Network.Topics.Raw where
 
 import           Control.Applicative (liftA2)
 import           Control.Monad.Free (Free(Free))
-import           Control.Monad.Free.Class (MonadFree)
-import           Control.Monad.Reader (ReaderT)
-import           Control.Monad.Reader.Class (MonadReader)
+import           Control.Monad.Free.Class (MonadFree, liftF)
+import           Control.Monad.Reader (ReaderT, runReaderT)
+import           Control.Monad.Reader.Class (MonadReader, asks)
+
+import           Network.Topics
 
 -- thoughts:
 --
@@ -68,6 +73,10 @@ instance Applicative RawProg where
           (Free rrpf, Free rrpa) -> Free (liftA2 (<*>) rrpf rrpa)
           _                      -> freeF <*> freeA
 
+newtype RawPart v a =
+    RawPart (ReaderT (Topic v, PartitionId) Raw a)
+  deriving (Functor, Applicative, Monad, MonadFree RawReq, MonadReader (Topic v, PartitionId))
+
 -- TODO have this in it's own module, with protected constructors maintaining type safety
 data RawReq a =
     RawReq { requests :: [KafkaReq], respond :: [KafkaResp] -> a }
@@ -83,6 +92,42 @@ instance Applicative RawReq where
             -- TODO check number of responses
             let (lResponses, rResponses) = splitAt (length (requests left)) responses
             in  respond left lResponses (respond right rResponses))
+
+instance Topics Raw RawPart where
+    -- TODO need actual kafka requests and responses. Once we have real requests
+    --      and responses, note that we may need to know v in order to pass in
+    --      appropriate serializers and deserializers ... or more likely, most of
+    --      the safe constructors for RawReq will require v to be Kafkaesque.
+    -- TODO I think I need the ability to throw an error here if response is for the
+    --      wrong topic, as whatever actually "runs" the Raw program will only be
+    --      responsible for creating the KafkaResp value
+    getTopic :: Kafkaesque v => TopicName -> Raw (Topic v)
+    getTopic topic =
+        liftF (RawReq [PlaceholderKafkaReq] (const (Topic topic undefined)))
+
+    withPartitions :: Kafkaesque v => Topic v -> [PartitionId] -> RawPart v a -> Raw [a]
+    withPartitions topic pIds (RawPart action) =
+        traverse (\pId -> runReaderT action (topic, pId)) pIds
+
+instance Kafkaesque v => Partition RawPart v where
+    partitionId :: RawPart v PartitionId
+    partitionId = asks snd
+
+    -- TODO need actual kafka requests and responses. Will need to ask for the
+    --      topic and partition id in order to create requests.
+
+    getOffsets :: RawPart v (Offset, Offset)
+    getOffsets =
+      liftF (RawReq [PlaceholderKafkaReq] (const undefined))
+
+    produce :: [v] -> RawPart v Offset
+    produce items =
+      liftF (RawReq [PlaceholderKafkaReq] (const undefined))
+
+
+    fetch :: Offset -> Size -> RawPart v (FetchData v)
+    fetch offset size =
+      liftF (RawReq [PlaceholderKafkaReq] (const undefined))
 
 {-
 runRaw :: Raw a -> RawConf -> EitherT RawError IO a
